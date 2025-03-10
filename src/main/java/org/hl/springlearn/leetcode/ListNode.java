eb6e200ecaaa8f2c6bfd3c89f603812d3cba703c
toutudeHL <houlei0525@163.com>
2025-03-10 21:31:18 +0800

    add leetcode；

---
+++ src/main/java/org/hl/springlearn/leetcode/ListNode.java
@@ -0,0 +1,29 @@
+package org.hl.springlearn.leetcode;
+
+/**
+ * 通用简单链表节点
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    void print() {
+        ListNode listNode = this;
+        while (listNode != null) {
+            System.out.print(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+}
